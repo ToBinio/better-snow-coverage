87147aa52a42d0961980c747127ecc0380449e1f
tobinio <Tobias.frischmann1@gmail.com>
2024-10-23 14:49:03 +0200

    fix typos

--- src/main/java/tobinio/bettersnowcoverage/mixin/ChunkBuilderMixin.java
+++ src/main/java/tobinio/bettersnowcoverage/mixin/ChunkBuilderMixin.java
@@ -1,7 +1,6 @@
 package tobinio.bettersnowcoverage.mixin;
 
 import com.llamalad7.mixinextras.sugar.Local;
-import com.mojang.blaze3d.systems.VertexSorter;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.client.render.BufferBuilder;
@@ -11,12 +10,8 @@ import net.minecraft.client.render.chunk.ChunkBuilder;
 import net.minecraft.client.render.chunk.ChunkRendererRegion;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.ChunkSectionPos;
 import net.minecraft.util.math.random.Random;
-import org.jetbrains.annotations.Nullable;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
@@ -29,7 +24,7 @@ import tobinio.bettersnowcoverage.BetterSnowChecker;
  * @author Tobias Frischmann
  */
 @Mixin (ChunkBuilder.BuiltChunk.RebuildTask.class)
-public class SectionBuilderMixin {
+public class ChunkBuilderMixin {
 
     @Inject (method = "render", at = @At (value = "INVOKE", target = "Lnet/minecraft/client/render/block/BlockRenderManager;renderBlock(Lnet/minecraft/block/BlockState;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/world/BlockRenderView;Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumer;ZLnet/minecraft/util/math/random/Random;)V"))
     private void render(float cameraX, float cameraY, float cameraZ, BlockBufferBuilderStorage storage,
