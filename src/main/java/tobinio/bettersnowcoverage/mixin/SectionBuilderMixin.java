cca310842ad92c53cc6bfa2f84eeb8bfb2275772
tobinio <Tobias.frischmann1@gmail.com>
2025-03-27 20:07:01 +0100

    support version 1.21.5

--- src/main/java/tobinio/bettersnowcoverage/mixin/SectionBuilderMixin.java
+++ src/main/java/tobinio/bettersnowcoverage/mixin/SectionBuilderMixin.java
@@ -2,17 +2,17 @@ package tobinio.bettersnowcoverage.mixin;
 
 import com.llamalad7.mixinextras.sugar.Local;
 import com.mojang.blaze3d.systems.VertexSorter;
+import net.fabricmc.fabric.impl.client.indigo.renderer.accessor.AccessChunkRendererRegion;
+import net.minecraft.block.BlockRenderType;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
-import net.minecraft.client.render.BufferBuilder;
 import net.minecraft.client.render.block.BlockRenderManager;
 import net.minecraft.client.render.chunk.BlockBufferAllocatorStorage;
 import net.minecraft.client.render.chunk.ChunkRendererRegion;
 import net.minecraft.client.render.chunk.SectionBuilder;
-import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.client.render.model.BlockStateModel;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.ChunkSectionPos;
-import net.minecraft.util.math.random.Random;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -33,19 +33,18 @@ public class SectionBuilderMixin {
     @Final
     private BlockRenderManager blockRenderManager;
 
-    @Inject (method = "build", at = @At (value = "INVOKE", target = "Lnet/minecraft/client/render/block/BlockRenderManager;renderBlock(Lnet/minecraft/block/BlockState;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/world/BlockRenderView;Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumer;ZLnet/minecraft/util/math/random/Random;)V"))
-    private void betterSnowCoverage$render(ChunkSectionPos sectionPos, ChunkRendererRegion renderRegion, VertexSorter vertexSorter,
+    @Inject (method = "build", at = @At (value = "INVOKE", target = "Lnet/minecraft/block/BlockState;getRenderType()Lnet/minecraft/block/BlockRenderType;"))
+    private void betterSnowCoverage$render(ChunkSectionPos sectionPos, ChunkRendererRegion renderRegion,
+            VertexSorter vertexSorter,
             BlockBufferAllocatorStorage allocatorStorage, CallbackInfoReturnable<SectionBuilder.RenderData> cir,
-            @Local BlockState blockState, @Local (ordinal = 2) BlockPos blockPos, @Local MatrixStack matrixStack,
-            @Local BufferBuilder bufferBuilder, @Local Random random) {
+            @Local BlockState blockState, @Local (ordinal = 2) BlockPos blockPos) {
 
-        var snowState = BetterSnowChecker.shouldHaveSnow(blockPos.up());
+        if (blockState.getRenderType() == BlockRenderType.MODEL && BetterSnowChecker.shouldHaveSnow(blockPos.up()) == BetterSnowChecker.SnowState.WITH_LAYER ) {
+            var snowBlockPos = blockPos.up();
+            var snowBlockState = Blocks.SNOW.getDefaultState();
+            BlockStateModel model = blockRenderManager.getModel(snowBlockState);
 
-        if (snowState == BetterSnowChecker.SnowState.WITH_LAYER) {
-            matrixStack.push();
-            matrixStack.translate(0, 1, 0);
-            blockRenderManager.renderBlock(Blocks.SNOW.getDefaultState(), blockPos.up(), renderRegion, matrixStack, bufferBuilder, true, random);
-            matrixStack.pop();
+            ((AccessChunkRendererRegion) renderRegion).fabric_getRenderer().bufferModel(model, snowBlockState, snowBlockPos);
         }
     }
 
